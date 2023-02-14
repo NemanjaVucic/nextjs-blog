c1032f0088aab526360173a4466334b2ae9118ee
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-02-15 00:04:21 +0100

    feat: small changes just to see deployment preview

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ export default function Home({ allPosts }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={utilStyles.headingMd}>Read Carefully</section>
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
